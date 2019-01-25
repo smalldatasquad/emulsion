b20d9b6bb7dbc69d1edd57e4bc87d98df3037108
Dan Taeyoung <you@example.com>
2019-01-25 15:09:45 -0500

    works with new Google Takeout as of 2019 Jan

--- google_1_simplify_queries.py
+++ google_1_simplify_queries.py
@@ -1,7 +1,10 @@
+import dateutil.parser
+import re
 import csv
 import os, errno
 import io
 import json
+import sys
 from pprint import pprint
 from datetime import datetime
 import argparse
@@ -22,22 +25,27 @@ args = vars(parser.parse_args())
 
 
 ############## get all filenames
-path_to_json = 'Searches/' if args['dir'] == None else args['dir']
+path_to_json = 'TakeoutData/' if args['dir'] == None else args['dir']
 
 print(" ")
 print(" ===  Reading JSON files in '" + path_to_json + "'...")
 
-json_files = [os.path.join(root, f) for root, subdirs, files in os.walk("Searches") for f in files if f.endswith('.json')]
+json_files = [os.path.join(root, f) for root, subdirs, files in os.walk(path_to_json) for f in files if f.endswith('.json')]
 
 print(" ===  Combining JSON files...",)
+
+if(len(json_files) == 0):
+    print("There are no JSON files in " + path_to_json + "!")
+    sys.exit(0)
 
 ############# concat all jsons into list (let's hope this doesn't break on large numbers of json)
 alljsons = []
 for f in json_files:
-    print(".",)
+    print("..processing..",)
     with open(f) as data:
         d = json.load(data)
-        alljsons.extend( d['event'] )
+        alljsons.extend(d)
+
 
 print("")
 print(" ===  Creating simple JSON...")
@@ -45,21 +53,25 @@ print(" ===  Creating simple JSON...")
 ############ make a super simple json
 simplejson = []
 for j in alljsons:
-    single_query = {}
-    single_query["data_query"] = j['query']['query_text']
-    single_query["timestamp_usec"] = j['query']['id'][0]['timestamp_usec']
-    d = timestamp_usec_to_datetime(single_query["timestamp_usec"])
-    single_query["datetime"] = d.strftime("%m/%d/%Y %I:%M:%S %p")
-    single_query["month"] = int(d.strftime("%m"))
-    single_query["day"] = int(d.strftime("%d"))
-    single_query["year"] = int(d.strftime("%Y"))
-    single_query["hour"] = int(d.strftime("%H"))
-    single_query["minute"] = int(d.strftime("%M"))
-    single_query["second"] = int(d.strftime("%S"))
-    single_query["weekofyear"] = int(d.strftime("%U"))
-    single_query["weekday"] = d.strftime("%a")
-    single_query["dayofmonth"] = int(d.strftime("%d"))
-    simplejson.append(single_query)
+
+#    print(j)
+    m = re.search('Searched for (.*)', j['title'])
+    if(m):
+        single_query = {}
+        single_query["data_query"] =  m.group(1)
+        d = dateutil.parser.parse(j['time'])
+        single_query["timestamp_usec"] = d.timestamp()
+        single_query["datetime"] = d.strftime("%m/%d/%Y %I:%M:%S %p")
+        single_query["month"] = int(d.strftime("%m"))
+        single_query["day"] = int(d.strftime("%d"))
+        single_query["year"] = int(d.strftime("%Y"))
+        single_query["hour"] = int(d.strftime("%H"))
+        single_query["minute"] = int(d.strftime("%M"))
+        single_query["second"] = int(d.strftime("%S"))
+        single_query["weekofyear"] = int(d.strftime("%U"))
+        single_query["weekday"] = d.strftime("%a")
+        single_query["dayofmonth"] = int(d.strftime("%d"))
+        simplejson.append(single_query)
 
 
 ############ sort json into chronological order
--- helpers.py
+++ helpers.py
@@ -6,12 +6,13 @@ import json
 fieldnames = ["timestamp_usec", "datetime","month", "day", "year", "hour", "minute", "second", "weekofyear", "weekday", "dayofmonth" ,"data_query"]
 
 def csvsave(filename, data):
-    with open(filename, 'w') as fout:
+    with open(filename, 'wb') as fout:
         fout.write(u'\ufeff'.encode('utf8')) #  (optional...Excel needs it to open UTF-8 file properly)
+    with open(filename, 'w') as fout:
         writer = csv.DictWriter(fout, fieldnames=fieldnames, delimiter="\t")    
         writer.writeheader()
         for row in data[1:]:
-            writer.writerow({k:unicode(v).encode('utf8') for k,v in row.items() })
+            writer.writerow({k:str(v).encode('utf8') for k,v in row.items() })
 
 def jsonsave(filename, data):
     with io.open(filename, 'w', encoding='utf-8') as f:
